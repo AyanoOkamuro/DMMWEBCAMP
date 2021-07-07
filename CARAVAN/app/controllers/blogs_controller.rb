d33df55f6f847fae186410be5d1123e44d15207d
AyanoOkamuro <omochi185@gmail.com>
2021-07-07 05:16:33 +0000

    [finish]3-4

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   
   def index
+    @blogs = Blog.all
   end
 
   def show
@@ -23,4 +24,4 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  end
+end
