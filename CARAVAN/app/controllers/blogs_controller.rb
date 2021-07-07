ebf67c4803baddc68c434d6b22939b5bb8e73bd8
AyanoOkamuro <omochi185@gmail.com>
2021-07-07 05:01:15 +0000

    [finish]3-3

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -11,9 +11,16 @@ class BlogsController < ApplicationController
   end
   
   def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
   
-end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+  end
