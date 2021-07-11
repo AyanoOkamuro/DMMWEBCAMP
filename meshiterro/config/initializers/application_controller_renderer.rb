e877c80f5485f5c4a8db6bf42b1643d039028afd
AyanoOkamuro <omochi185@gmail.com>
2021-07-11 10:13:07 +0000

    [finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '200db6c917a9140e217b51a67ca1cebcba7b7d7650f30acb1b0b54988267d6a812186d1de7c9d692ac2e07f59dd825fdbbac669d8fb2faf3e58a4aa8c03c2765'
